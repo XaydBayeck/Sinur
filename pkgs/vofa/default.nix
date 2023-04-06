927ef526ed03ab63ff78986441204db8075b37f0
Sid <SidBayeck@outlook.com>
2023-04-06 20:38:41 +0800

    feat(pkgs/vofa): Add new pkg vofa+
    Vofa+ is a Plug-in-driven high-degree-of-freedom upper computer.
    Used to display data send from usart of Embedded system with
    Rich graphic.

--- pkgs/vofa/default.nix
+++ pkgs/vofa/default.nix
@@ -1,22 +1,65 @@
-{
-  stdenv,
-  fetchurl,
-  autoPatchelfHook,
-  # lib,
-  makeWrapper,
-  # callPackage,
-  ...
+{ stdenv
+, fetchurl
+, autoPatchelfHook
+, # lib,
+  makeWrapper
+, # callPackage,
+  libxkbcommon
+, pango
+, libXext
+, cairo
+, gdk_pixbuf
+, dbus
+, libglvnd
+, libX11
+, systemd
+, # turn 2
+  gtk3
+, at-spi2-atk
+, openssl
+, libjpeg_original
+, libICE
+, libSM
+, ...
 }:
 
+let
+  libjpeg_original-v8d = libjpeg_original.overrideAttrs (old: rec {
+    pname = "libjpeg-v8d";
+    version = "8d";
+    src = fetchurl {
+      url = "http://www.ijg.org/files/jpegsrc.v${version}.tar.gz";
+      sha256 = "sha256-/cTUwRM4rQKKfSP7U/W7k1RnE5Kmf7G1LgwypxIYkfg=";
+    };
+  });
+  libraries = [
+    libxkbcommon
+    pango
+    libXext
+    cairo
+    gdk_pixbuf
+    dbus
+    libglvnd
+    libX11
+    systemd
+    gtk3
+    at-spi2-atk
+    openssl
+    libICE
+    libSM
+    libjpeg_original-v8d
+  ];
+in
 stdenv.mkDerivation rec {
   pname = "vofa+";
   version = "1.3.10";
   src = fetchurl {
     url = "https://je00.github.io/downloads/vofa+_${version}_amd64.deb";
-    sha256 = "";
+    sha256 = "sha256-uwQsWzpXd9gn53MfwAhAw9YbzUWoe6nFAt1UBkIR6AA=";
   };
 
   nativeBuildInputs = [ autoPatchelfHook makeWrapper ];
+  buildInputs = libraries;
 
   unpackPhase = ''
     ar x ${src}
@@ -26,6 +69,10 @@ stdenv.mkDerivation rec {
   installPhase = ''
     mkdir -p $out
     mv opt $out/
-    mv user/share $out/
+    mv usr/share $out/
+
+    # 替换菜单项目（desktop 文件）中的路径
+    sed -i "s|Exec=.*|Exec=$out/opt/vofa+/vofa+|" $out/share/applications/*.desktop
+    sed -i "s|Icon=.*|Icon=$out/opt/vofa+/vofa+.png|" $out/share/applications/*.desktop
   '';
 }
