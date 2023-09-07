69485e170ec7bf5674f707299850e698bd6f1b84
Sid <SidBayeck@outlook.com>
2023-09-07 13:36:13 +0800

    update stable nixos version and basic file

--- flake.nix
+++ flake.nix
@@ -16,8 +16,11 @@
       forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
     in
     {
-      packages = forAllSystems (system: import ./default.nix {
+      legacyPackages = forAllSystems (system: import ./default.nix {
         pkgs = import nixpkgs { inherit system; };
       });
+      packages = forAllSystems (systems:
+        nixpkgs.lib.filterAttrs
+          (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${systems});
     };
 }
